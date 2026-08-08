506355eefddf7f8a59639bc09f80fd6a0ee30c7d
schoolab4-byte <schoolab4@gmail.com>
2026-08-08 10:57:04 +0530

    Implement digital clock and notes storage
    Added digital clock and notes saving functionality.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+// Digital Clock
 
+function updateClock(){
+
+let time=new Date();
+
+document.getElementById("clock").innerHTML =
+time.toLocaleTimeString();
+
+}
+
+setInterval(updateClock,1000);
+
+
+// Notes Save
+
+function saveNotes(){
+
+let data=document.getElementById("notes").value;
+
+localStorage.setItem("studentNotes",data);
+
+alert("Notes Saved!");
+
+}
+
+
+// Load Notes
+
+window.onload=function(){
+
+document.getElementById("notes").value =
+localStorage.getItem("studentNotes") || "";
+
+}
