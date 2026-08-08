60f1aee98d43d11eea19c23cc79a6efdab85a434
schoolab4-byte <schoolab4@gmail.com>
2026-08-08 11:00:05 +0530

    Implement basic calculator functions in script.js

--- script.js
+++ script.js
@@ -33,3 +33,42 @@ document.getElementById("notes").value =
 localStorage.getItem("studentNotes") || "";
 
 }
+
+let expression="";
+
+
+function add(value){
+
+expression += value;
+
+document.getElementById("display").value = expression;
+
+}
+
+
+function clearDisplay(){
+
+expression="";
+
+document.getElementById("display").value="";
+
+}
+
+
+function calculate(){
+
+try{
+
+expression = eval(expression);
+
+document.getElementById("display").value = expression;
+
+}
+
+catch{
+
+document.getElementById("display").value="Error";
+
+}
+
+}
